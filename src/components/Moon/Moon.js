2207f582ae49826843731c328baa010ae6ae4ebe
deanssmart <deanssmart@gmail.com>
2020-12-05 13:12:11 +0000

    moon tecture removed as it was breaking the loading

--- src/components/Moon/Moon.js
+++ src/components/Moon/Moon.js
@@ -1,22 +1,17 @@
-import React, { useMemo } from 'react';
-import * as THREE from 'three';
+import React from 'react';
 
 const Moon = ({ position }) => {
-
-    const texture = useMemo(() => new THREE.TextureLoader().load("assets/Textures/Moon/Moon.jpg"), []);
 
     return (
         
         <mesh position={position} >
-            <sphereBufferGeometry attach="geometry" args={[7, 20, 20]} />
+            <sphereBufferGeometry attach="geometry" args={[5, 20, 20]} />
             <meshPhysicalMaterial 
                 attach="material" 
                 color="white"
-                // metalness={2}
+                metalness={2}
                 fog={false}
-            >
-                <primitive attach="map" object={texture} />
-            </meshPhysicalMaterial>
+            />
         </mesh>
     );
 }
