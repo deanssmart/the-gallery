f51dba137f51fd07eda3fb953b3a2dd2f816aa99
Roussetos Karafyllakis <84732055+gene-roussetos@users.noreply.github.com>
2022-07-12 19:59:31 +0100

    gitignore and test fix import

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './components/App/App';
 
 test('renders learn react link', () => {
   render(<App />);
